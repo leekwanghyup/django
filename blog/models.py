8a0ffa0315271415796146d0dc485f90a2c26b24
leekwanghyup <leekwanghyup@naver.com>
2023-04-23 21:25:25 +0900

    Post 모델ì 생성, ì간 설정

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()  # 문자열의 길이 제한 없음
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    # author: 추후 작성
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
